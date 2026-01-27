b9ab706ca3f9594a6e5f672775af98019c3ec6ff
QDnh <qdonahe1@buffs.wtamu.edu>
2026-01-26 20:06:47 -0600

    Upload Homework1

---
+++ Homework1.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Define variables and values.
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = (4*X*X + 3*Y);
+        
+        // Output values in desired format.
+        Console.WriteLine($"X = {X}, Y = {Y}");
+        Console.WriteLine($"The value of Z is: {Z}");
+    }
+}
